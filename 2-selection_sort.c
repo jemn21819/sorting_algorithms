9f4a9353188084d6451dae657270265b24ac1c6f
James Nieves <jmartinez21734@stu.nuc.edu>
2021-01-30 13:24:38 +0000

    adding task 2 files

---
+++ 2-selection_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending
+ * order using the Selection sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ * Return: void
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i = 0, j = 0, min, temp;
+
+	for (i = 0; i < size -1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
+}
