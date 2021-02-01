7fb7de620b81c627fede05e5cd71d9ba87dca56e
James Nieves <jmartinez21734@stu.nuc.edu>
2021-02-01 11:51:59 +0000

    betty fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,18 +5,24 @@
  * Bubble sort algorithm
  * @array: array to be sorted by fucntion
  * @size: Size of  the array to be sorted
+ * Return: Void
  */
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i = 0, j = 0, tmp;
+	size_t i = 0;
+	size_t j = 0;
+	size_t tmp;
 
 	for (i = 0; i < size; i++)
 	{
+		/*first loop of fisrt position*/
 		for (j = 0; j < size - 1; j++)
 		{
+			/*another loop for the last sorted*/
 			if (array[j] > array[j + 1])
 			{
+				/*swap elements*/
 				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
